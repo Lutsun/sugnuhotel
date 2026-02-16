63d83f1979d7f5e1a83bf98913512c07010bece5
Lutsun <sergedasylva0411@gmail.com>
2026-02-16 15:56:02 +0000

    update for seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+     public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            AdminUserSeeder::class,
+            RoomTypeSeeder::class,
+            RoomSeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
